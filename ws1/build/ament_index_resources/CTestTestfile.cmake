ee473155f503c36cb878a83894b727f15eeb21bc
Radu Popescu <radupopescu100@gmail.com>
2025-02-19 09:09:23 +0100

    update

--- ws1/build/ament_index_resources/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/radu/rapop/ament_playground/ws1/src/ament_index_resources
-# Build directory: /home/radu/rapop/ament_playground/ws1/build/ament_index_resources
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
